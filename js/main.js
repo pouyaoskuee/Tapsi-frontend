86c16ea4cd966b6f004d04a0c2b5aff520697a9c
pouya <pouyakhaleghi82@gmail.com>
2025-11-30 02:13:11 +0330

    The components were connected.

--- js/main.js
+++ js/main.js
@@ -18,3 +18,30 @@ function open_menu() {
     hamburger_three.classList.toggle('rotation-three');
 
 }
+
+
+
+const tabs = document.querySelectorAll(".tabs__tab");
+const tabContents = document.querySelectorAll(".tabs__result__content");
+
+
+
+tabs.forEach((tab) => {
+    tab.addEventListener("click", (e) => {
+        e.preventDefault();
+        const targeTabContent = document.querySelector(`#${tab.dataset.tabTarget}`);
+
+        // REMOVE PREVIOUS ACTIVED CLASSES
+        tabs.forEach((tab) => tab.classList.remove("active"));
+        tabContents.forEach((tabContent) => tabContent.classList.remove("active"));
+
+        // ADD NEW ACTIVE CLASSES
+        tab.classList.add("active");
+        targeTabContent.classList.add("active");
+    });
+});
+
+
+
+
+
